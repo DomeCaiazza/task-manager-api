a7da5ba4e1c260a132111eebb8685531b0d01f41
Domenico Caiazza <caiazza.domenico@gmail.com>
2025-06-11 21:48:09 +0200

    refactor Docker setup to support separate development and testing databases

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -4,5 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails
 rm -f /app/tmp/pids/server.pid
 
+# Install gems
+bundle install
+
+rails db:create
+rails db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile)
 exec "$@" 
